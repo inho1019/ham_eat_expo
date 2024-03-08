e485e94526769e6fd689dbe833fffd1887f851cd
simin <inho1019@naver.com>
2024-03-08 13:02:17 +0900

    베타 완성 진짜

--- components/api/ContextAPI.jsx
+++ components/api/ContextAPI.jsx
@@ -7,7 +7,7 @@ import { Dimensions, Image, Text, View } from 'react-native';
 // 초기 상태
 const initialState = {
   loading : false,
-  version : '0.1.7',
+  version : '0.1.8',
   page: 0,
   alertTxt: '',
   date: new Date(),
